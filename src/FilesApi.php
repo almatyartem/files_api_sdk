837c83cb5057dd9daecc44fc85c4c0d65b810a26
Artem Golodov <almatyartem@gmail.com>
2020-05-05 23:07:14 +0600

    save

--- src/FilesApi.php
+++ src/FilesApi.php
@@ -47,18 +47,20 @@ class FilesApi
     /**
      * @param string $url
      * @param string $dir
+     * @param array|null $stopPhrases
      * @param string|null $fileName
      * @param string|null $fileExt
      * @return string|null
      * @throws RequestProviderException
      */
-    public function uploadImageByUrl(string $url, string $dir, string $fileName = null, string $fileExt = null) : ?string
+    public function uploadImageByUrl(string $url, string $dir, array $stopPhrases = null, string $fileName = null, string $fileExt = null) : ?string
     {
         $result = $this->provider->request($this->api , 'post','images/add', [
             'download_url' => $url,
             'dir' => $dir,
             'file_name' => $fileName,
-            'file_ext' => $fileExt
+            'file_ext' => $fileExt,
+            'stop_phrases' => $stopPhrases
         ]);
 
         return $result['code'] ?? null;
